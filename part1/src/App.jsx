b764542ce96f9743508a48b674e201402d6c56c7
willgpennell <wpennell48@gmail.com>
2024-10-01 16:57:04 -0230

    part1

---
+++ part1/src/App.jsx
@@ -0,0 +1,25 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const now = new Date();
+  const name = "Peter";
+  const age = 10;
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <Hello name="hi" />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+};
+export default App;
