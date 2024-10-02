252687a5bdbad637d25591d841ec3613bdcb0a7c
willgpennell <wpennell48@gmail.com>
2024-10-02 12:00:52 -0230

    part1 courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -13,14 +13,9 @@ const App = () => {
   return (
     <div>
       <Header />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
 
       <Excercises
         exercises1={exercises1}
