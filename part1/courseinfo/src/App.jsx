b9680dbd5bfd28970e5a2f169904f0bed19969e2
willgpennell <wpennell48@gmail.com>
2024-10-02 11:58:59 -0230

    part1 courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,34 @@
+import Header from "./Header";
+import Content from "./Content";
+import Excercises from "./Excercises";
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-}
+      <Header />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
 
-export default App
+      <Excercises
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ part1/courseinfo/src/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  const course = "Half Stack application development";
+
+  return <h1>{course}</h1>;
+};
+
+export default Header;
